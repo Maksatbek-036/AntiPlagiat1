f67f0c7e3ca8ba8ee1e442d747c68626474ae119
Maks <161190918+Maksatbek-036@users.noreply.github.com>
2026-02-14 00:05:21 +0600

    menu кнопки

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
 }
 
 android {
@@ -31,6 +32,12 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+    buildFeatures {
+        viewBinding =true
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
 }
 
 dependencies {
@@ -39,7 +46,15 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.core.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+
+    // Конвертер для работы с JSON (GSON)
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+
+    // Логирование запросов (полезно при разработке)
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
 }
